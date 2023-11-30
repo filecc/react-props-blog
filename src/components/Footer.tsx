6c5236e42810417d2d576742081697de154881a3
filecc <fil.gia@live.it>
2023-11-30 15:49:58 +0100

    Refactor Footer and PostThumbnail components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,7 @@ export default function Footer({ isFocused } : { isFocused: boolean}) {
     <>
       <footer className={`grid place-items-center bg-slate-700 px-6 py-3 text-center text-slate-50 ${isFocused && 'blur-lg'}`}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
-        quibusdam, culpa id sint quisquam minima sit beatae natus eaque.
-        Consequatur atque expedita pariatur assumenda illum, delectus qui
-        adipisci mollitia exercitationem?
+        quibusdam.
       </footer>
     </>
   );
--- src/components/PostThumbnail.tsx
+++ src/components/PostThumbnail.tsx
@@ -3,13 +3,13 @@ import { PostType } from "../lib/data.types";
 export default function PostThumbnail({ post, setPost, setIsfocused } : { post: PostType, setPost: (post: PostType) => void, setIsfocused: (value:boolean) => void}){
     return (
         <>
-        <div className="md:h-fit md:w-[200px] rounded-b-lg h-52 bg-gray-50">
+        <div className="md:h-auto md:w-[200px] rounded-b-lg h-fit bg-gray-50">
             <div className=" max-h-24 overflow-hidden">
                 <img className="object-cover rounded-lg w-full" src={post.image} alt={post.title + ' image'} />
             </div>
-           <div className="px-2 py-3">
+           <div className="px-4 pt-2">
              <h2 className="text-2xl font-semibold">{post.title}</h2>
-             <p className="mt-2">{post.content.slice(0, 25) + '...'} <button onClick={()=>{
+             <p className="mt-2 pb-3">{post.content.slice(0, 80) + '...'} <button onClick={()=>{
                 setPost(post)
                 setIsfocused(true)
                 }} className="text-xs text-sky-500 hover:text-sky-700 hover:font-bold transition-colors duration-300">READ MORE</button></p>
